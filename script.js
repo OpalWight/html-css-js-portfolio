2e1b9352bab6322799871d34df15c5c39d9abc1f
OpalWight <albert.t.vo1@gmail.com>
2024-07-11 17:20:21 -0700

    GOT THE HUMBURGER MENU WORKING
    JS was selecting wrong thing.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".hamburger-menu").classList.toggle("active");
-    const icon = document.querySelector(".menu-links").classList.toggle("active");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
